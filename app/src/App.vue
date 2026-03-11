c67fb43107fa2e61accff4da9a90c9b3be21685d
Andy-Zhang7223 <andyz127@nycstudents.net>
2026-03-11 12:32:18 -0400

    3/11/2026

---
+++ app/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+//Asbestos data >:D :
+// https://data.cityofnewyork.us/api/v3/views/vq35-j9qm/query.json
+
+//Park Trails :D :
+// https://data.cityofnewyork.us/api/v3/views/vjbm-hsyr/query.json
+</script>
+
+<template>
+  <h1>You did it!</h1>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+</template>
+
+<style scoped></style>
